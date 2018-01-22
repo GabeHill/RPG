886c1731d0556b3e0ea84103a41e90065a115cdc
owner <owner@archlinux>
2018-01-22 16:48:55 -0700

    mod settings

--- src/headers/Being.h
+++ src/headers/Being.h
@@ -16,14 +16,14 @@ public:
 	Being(int i, int s, int d, std::string n);
 	Being(std::string n);
 
-	void changeIntelligence(int changeBy);
-	void changeStrength(int changeBy);
-	void changeDexterity(int changeBy);
-	void changeHealth(int changeBy);
-	void changeMana(int changeBy);
-	void changeArmor(int changeBy);
-	void setMaxHealth(int max);
-	void setMaxMana(int max);
+	void changeIntelligence(int changeBy)=0;
+	void changeStrength(int changeBy)=0;
+	void changeDexterity(int changeBy)=0;
+	void changeHealth(int changeBy)=0;
+	void changeMana(int changeBy)=0;
+	void changeArmor(int changeBy)=0;
+	void setMaxHealth(int max)=0;
+	void setMaxMana(int max)=0;
 
 	int getIntelligence() const;
 	int getIntelMod() const;
@@ -44,7 +44,81 @@ protected:
 			armorClass;
 
 private:
-	void setModifiers();
+	void setModifiers() {
+		intelligenceMod = setMod(getIntelligence());
+		strengthMod = setMod(getStrength());
+		dexterityMod = setMod(getDexterity());
+	}
+	int setMod(int stat) {
+		int mod;
+		switch (stat) {
+		case 1:
+			mod = -5;
+			break;
+		case 2:
+		case 3:
+			mod = -4;
+			break;
+		case 4:
+		case 5:
+			mod = -3;
+			break;
+		case 6:
+		case 7:
+			mod = -2;
+			break;
+		case 8:
+		case 9:
+			mod = -1;
+			break;
+		case 10:
+		case 11:
+			mod = 0;
+			break;
+		case 12:
+		case 13:
+			mod = 1;
+			break;
+		case 14:
+		case 15:
+			mod = 2;
+			break;
+		case 16:
+		case 17:
+			mod = 3;
+			break;
+		case 18:
+		case 19:
+			mod = 4;
+			break;
+		case 20:
+		case 21:
+			mod = 5;
+			break;
+		case 22:
+		case 23:
+			mod = 6;
+			break;
+		case 24:
+		case 25:
+			mod = 7;
+			break;
+		case 26:
+		case 27:
+			mod = 8;
+			break;
+		case 28:
+		case 29:
+			mod = 9;
+			break;
+		case 30:
+			mod = 105;
+			break;
+		default:
+			break;
+		}
+		return mod;
+	}
 
 };
 
